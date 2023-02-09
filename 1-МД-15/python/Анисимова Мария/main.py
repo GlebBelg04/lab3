99215c96d5b101b9076bb61eb905ff9fcbeec3a0
VeronikaKurilovich <kurilovichveronika@gmail.com>
2023-02-09 17:21:06 +0300

    change color

--- 1-МД-15/python/Анисимова Мария/main.py
+++ 1-МД-15/python/Анисимова Мария/main.py
@@ -33,7 +33,7 @@ def move():
     head.move(aim)
 
     if not inside(head) or head in snake:
-        square(head.x, head.y, 9, 'red')
+        square(head.x, head.y, 9, 'brown')
         update()
         return
 
@@ -51,7 +51,7 @@ def move():
     for body in snake:
         square(body.x, body.y, 9, 'black')
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, 'brown')
     update()
     ontimer(move, 100)
 
